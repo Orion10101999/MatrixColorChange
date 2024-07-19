ef4450f3157cd6b3142d1a2db55c2b97582903a8
Sandeep Kumar <orion10101999@gmail.com>
2024-07-19 16:34:14 +0530

    practice

--- src/components/Matrix.jsx
+++ src/components/Matrix.jsx
@@ -1,65 +1,9 @@
-
-import React, { useEffect, useState } from 'react';
+import React from 'react'
 
 const Matrix = () => {
-    const [matrix, setMatrix] = useState(Array(9).fill('white'));
-    const [clickOrder, setClickOrder] = useState([]);
-    const [auto, setAuto] = useState(false)
-    const autoColorChange = () => {
-        setTimeout(() => {
-            const newMatrix = [...matrix];
-            clickOrder.forEach((item, i) => {
-                setTimeout(() => {
-                    newMatrix[item] = 'orange';
-                    setMatrix([...newMatrix]);
-                }, i * 500);
-            });
-        }, 1000);
-    };
-    const handleClick = (index) => {
-        if (index === 8 && matrix[index] === 'white') {
-            const newMatrix = [...matrix];
-            newMatrix[index] = 'green';
-            setMatrix(newMatrix);
-            setClickOrder([...clickOrder, index]);
-            setAuto(true);
-        } else if (matrix[index] === 'white') {
-            const newMatrix = [...matrix];
-            newMatrix[index] = 'green';
-            setMatrix(newMatrix);
-            setClickOrder([...clickOrder, index]);
-        }
-    };
-    useEffect(() => {
-        if (auto) {
-            autoColorChange();
-        }
-    }, [auto])
-    const resetHandler = () => {
-        setMatrix(Array(9).fill('white'));
-        setClickOrder([]);
-        setAuto(false);
-    }
-    return (
-        <div className='flex justify-center items-center'>
-            <div className="grid grid-cols-3 gap-1">
-                {matrix.map((color, index) => (
-                    <div
-                        key={index}
-                        className="w-20 h-20 border cursor-pointer"
-                        style={{ backgroundColor: color }}
-                        onClick={() => handleClick(index)}
-                    />
-                ))}
-                <div>
-                    <button
-                        onClick={resetHandler}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-
-        2 px-4 rounded">Reset</button>
-                </div>
-            </div>
-        </div>
-    );
-};
+  return (
+    <div>Matrix</div>
+  )
+}
 
-export default Matrix;
+export default Matrix
